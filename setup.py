62c891de307adb5c5f400dad5b92dcba80c1dbcb
Tomas Vitvar <tomas@vitvar.com>
2023-05-29 12:49:18 +0200

    new yamc server version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*parts):
 
 # setup main
 # required modules
-install_requires = ["yamc_server>=1.1.0", "paho-mqtt>=1.6.1", "setuptools_scm>=6.0.1"]
+install_requires = ["yamc_server>=2.0.0", "paho-mqtt>=1.6.1", "setuptools_scm>=6.0.1"]
 
 setup(
     name="yamc-mqtt",
